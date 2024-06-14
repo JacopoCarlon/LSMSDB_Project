276741dc59d45663a1b0e21a5533be1b3f994a3f
Ayoub El Ourrak <ayoubelourrak@protonmail.com>
2024-06-14 19:07:37 +0200

    inizio scheletro

---
+++ demo/src/test/java/com/AnimeDB/demo/AnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AnimeDB.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
