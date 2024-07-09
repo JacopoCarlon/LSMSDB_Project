b07ec4d8913b60078b4a0b7197b8a9e9e2e990c8
nickrick3 <nik3.riccardi@gmail.com>
2024-07-09 16:48:24 +0100

    Skeleton of LoginREST

---
+++ MyAnime/src/main/java/it/unipi/lsmd/MyAnime/controller/api/LoginREST.java
@@ -0,0 +1,54 @@
+package it.unipi.lsmd.MyAnime.controller.api;
+
+import com.google.common.hash.Hashing;
+import it.unipi.lsmd.MyAnime.model.User;
+import it.unipi.lsmd.MyAnime.repository.UserRepoMongoDB;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+
+@RestController
+public class LoginREST {
+    @Autowired
+    UserRepoMongoDB userRepoMongoDB;
+
+    @PostMapping("/api/login")
+    public @ResponseBody String login (HttpSession session,
+                                       @RequestParam("username") String username,
+                                       @RequestParam("password") String password) {
+
+        try {
+            User user = userRepoMongoDB.getUserByUsername(username);
+
+            if(user == null){
+                // TODO return not found
+                return "";     // User not found
+            }
+
+            String hashedPassword = Hashing.sha256()
+                    .hashString(password, StandardCharsets.UTF_8)
+                    .toString();
+
+            if(user.getPassword().equals(hashedPassword)){
+                // TODO return success
+                return "";     // Login successful
+            }
+            else {
+                // TODO return failure
+                return "";     // Wrong password
+            }
+
+        } catch (DataAccessResourceFailureException e) {
+            e.printStackTrace();
+            // TODO return error
+            return "";         // Error connecting to DB
+        }
+    }
+    }
+}
