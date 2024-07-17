3216c6c9b37b307ddfeae7e750e6a1cf76289df9
JacopoCarlon <jacopo.equi@gmail.com>
2024-07-17 12:35:25 +0200

    completed adminAddAnimePage html/js , need api backend java

--- MyAnime/src/main/resources/static/js/adminAddAnimePage.js
+++ MyAnime/src/main/resources/static/js/adminAddAnimePage.js
@@ -2,6 +2,7 @@
 //  TODO : unrelativize paths
 //  needs : controllers/api/...rest.java che abbia @PostMapping("/api/login") e mi ritorni outcome_code : 0
 $(document).ready(function () {
+
     $("#anime_upload_btn").click(function (e) {
         e.preventDefault();
         adminUploadAnime();
@@ -38,7 +39,9 @@ $(document).ready(function () {
         const episodes_val = $("#a_episodes_input").val();
         //  const airing_val = $("#a_airing_input").val();      // true/false
         const slider_airing_val = $('#a_airing_input').is(":checked")
-        const aired_val = $("#a_aired_input").val();
+        //  const aired_val = $("#a_aired_input").val();
+        const aired_input_from_val = $("#a_aired_input_from").val();
+        const aired_input_to_val = $("#a_aired_input_to").val();
         //  //  const averageScore_val = $("#a_averageScore_input").val();
         //  //  const scoredBy_val = $("#a_scoredBy_input").val();
         //  //  const watchers_val = $("#a_watchers_input").val();
@@ -51,19 +54,32 @@ $(document).ready(function () {
         const imgURL_val = $("#a_imgURL_input").val();
 
         //  alert("airing_val : " + airing_val);
-        alert("slider_airing_val : " + slider_airing_val);
+        //  alert("slider_airing_val : " + slider_airing_val);
 
-        if(!title_val || !source_val  || !imgURL_val ){
-            alert("title, source, airing and imgurl are obbligatory fields !!!");
+        //  alert(aired_input_from_val)
+        //  alert(aired_input_to_val)
+        //  alert(!aired_input_from_val)
+        //  alert(!aired_input_to_val)
+        if(!aired_input_from_val !== !aired_input_to_val){
+            alert("if you put one date, you must put both !!!!!!!!!!!!!!!")
+            return;
+        }
+        if(aired_input_from_val > aired_input_to_val){
+            alert("anime cannot time travel, check order of airing(from-to) dates")
             return;
         }
 
+        //  alert(episodes_val)
+        if(episodes_val <0){
+            // or 1 minimum (maybe future anime has zero idk)
+            alert("anime shall have at the least one episode ..?")
+            return;
+        }
 
-
-
-
-
-
+        if(!title_val || !source_val  || !imgURL_val ){
+            alert("title, source, airing and imgURL are obbligatory fields !!!");
+            return;
+        }
 
         let trgFilterNames = [ "type_cbox", "rating_cbox", "genre_cbox" ]
         let filters = [];
@@ -71,12 +87,41 @@ $(document).ready(function () {
             filters.push(getChosenByName(trgFilterNames[i_name]) );
         }
 
+        const formData = {
+            title_val               : title_val,
+            titleJapanese_val       : titleJapanese_val,
+            source_val              : source_val,
+            episodes_val            : episodes_val,
+            slider_airing_val       : slider_airing_val,
+            aired_input_from_val    : aired_input_from_val,
+            background_val          : background_val,
+            broadcast_val           : broadcast_val,
+            producer_val            : producer_val,
+            licensor_val            : licensor_val,
+            studio_val              : studio_val,
+            EpisodeDuration_val     : EpisodeDuration_val,
+            imgURL_val              : imgURL_val,
+            type_list               : filters[0],
+            rating_list             : filters[1],
+            genre_list              : filters[2]
+        };
+
+        alert("about to ajax request to upload anime")
+        $.ajax({
+            url: '/api/adminAnimeUpload',
+            data: formData,
+            dataType: 'json',
+            method: 'POST',
+
+            success: function(response) {
+                alert("anime successfully uploaded !")
+            },
+            error: function (xhr, status, error) {
+                alert("ERROR in ajax: " + error);
+            }
+        });
+
         return;
     }
-
-
-
-
-
 });
 
